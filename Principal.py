5f1683ab203f9d6c971833609e9485be361d82b1
VictorHugo3142 <38482687+VictorHugo3142@users.noreply.github.com>
2018-10-12 10:23:39 -0500

    Programa realizado con nuestro docente de clase sobre la memoria dinamica

---
+++ Principal.py
@@ -0,0 +1,8 @@
+__autor__ = 'Victor Hugo Vergara Jr'
+
+promedio = 9.67
+otralista = []
+supermercado = ['fruta', 'agua' , 'refresco', 'pan', 'pastel']
+estructuradedatos = ['agustin pimentel', 'francisco pineda', 'enrique bello']
+precios = [12, 34, 45, 47]
+porcentajes = [.34, .56, .12, 1.2]
---
+++ listas.py
@@ -0,0 +1,35 @@
+__autor__ = 'Victor Hugo Vergara Jr'
+
+class memDinamica:
+    __lista = []
+
+    def __init__(self, list):
+        self.__lista = list
+
+    def getAutos(self):
+        return self.__listas
+
+     def recorrerArreglo(self):
+        for x in self.getAutos():
+            print (self.getAutos().index(x)+1, x)
+
+    def agregarelementoarray(self, elemento): #Metodo que permite agregar mas elementos a las listas al final de estas
+        self.__listas.append(elemento)
+
+    def ordenarListas(self):
+        self.__listas.sort()
+
+    def eliminarNElementos(self, num):
+        self.__listas.pop(num)
+
+    def eliminarElemento(self, elemento):
+        self.__listas.remove(elemento)
+
+    def insertarNPosicion(self, num, elemento):
+        self.__listas.insert(num, elemento)
+
+    def imprimirListas(self): #Este metodo puede imprimir si es llamado
+        print (self.__listas)
+
+    def eliminarElemento(self):
+        self.__listas.pop
